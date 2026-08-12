b2a9aa4fb871301a1d8351af0edd61ed91a8a2b5
Gabriella <gabipaz04@gmail.com>
2026-08-12 18:21:35 -0400

    script.js adicionado

---
+++ atividades-de-aula/aula_11-02-2026/script.js
@@ -0,0 +1,5 @@
+// document.querySelectorAll('[style]').forEach(elemento => elemento.getAttribute("style"));
+
+//para ver diretamente no console (adicionado depois)
+
+document.querySelectorAll('[style]').forEach(elemento => console.log(elemento.getAttribute("style")));
